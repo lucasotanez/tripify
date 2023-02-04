4f3cbea36cf0f031830eaf5eed14c5e22c9ee3f9
Lucas Otanez <lotanez@usc.edu>
2023-02-04 01:20:10 -0800

    some css changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,10 +8,15 @@ export default function Home() {
   return (
     <main>
       <Title>Tripify</Title>
-      <p>Welcome to Tripify</p>
+      <h1>Tripify</h1>
+      <p>Your musical voyage awaits</p>
       <form>
         <input type="text" placeholder="Enter "/>
       </form>
+      <div class="buttonbox">
+        <button>Plan your trip</button>
+        {/* <button>test2</button> */}
+      </div>
       <Login />
     </main>
   );
