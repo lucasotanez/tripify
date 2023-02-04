db3d570f8db1be6b5603bd264e3bd22e9dbc34bc
Riley Bruins <ribru17@hotmail.com>
2023-02-03 23:32:29 -0800

    added login component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { Title } from "solid-start";
+import Login from "../components/Login"
 
 export default function Home() {
   return (
     <main>
       <Title>Tripify</Title>
+      <p>Welcome to Tripify</p>
+      <form>
+        <input type="text" placeholder="Enter "/>
+      </form>
+      <Login />
     </main>
   );
 }
